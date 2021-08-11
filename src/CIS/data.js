8878f275c3b242cf098cfd9fd4989b9b4f5ae327
Charlie Ray <artem.chekin1997@gmail.com>
2021-08-11 13:03:57 +0300

    initial page structure

--- src/CIS/data.js
+++ src/CIS/data.js
@@ -1,15 +1,82 @@
 export const aspects = [
-  'red',
-  'yellow',
-  'green',
-  'moonWhite',
-  'yellow-flickering',
-  'green-flickering',
-  'two-yellows',
-  'two-yellows-flickering',
-  'yellow-green',
-  'yellow-green-flickering',
-  'yellow-moonWhite',
-  'yellow-moonWhite-flickering',
-  'three-yellows',
+  {
+    id: 1,
+    name: 'red',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 2,
+    name: 'yellow',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 3,
+    name: 'green',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 4,
+    name: 'moonWhite',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 5,
+    name: 'yellow-flickering',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 6,
+    name: 'green-flickering',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 7,
+    name: 'two-yellows',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 8,
+    name: 'two-yellows-flickering',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 9,
+    name: 'yellow-green',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 10,
+    name: 'yellow-green-flickering',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 11,
+    name: 'yellow-moonWhite',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 12,
+    name: 'yellow-moonWhite-flickering',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
+  {
+    id: 13,
+    name: 'three-yellows',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit nulla quam cupiditate esse inventore sunt veritatis cum ullam tempora maxime, excepturi voluptatem rem soluta dolorem corrupti impedit iusto delectus? Nobis pariatur culpa odio ea libero dolores harum eaque, eum veritatis similique mollitia ab! Quis, facilis. In reiciendis quibusdam ratione?',
+  },
 ];
+
+export const signs = [{ id: 1, sign: 'sign-name', description: 'description' }];
